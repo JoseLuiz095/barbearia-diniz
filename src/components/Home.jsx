2de381747b5979cf1585cc3c8f709dfe31c018f9
José Luiz <82607322+JoseLuiz095@users.noreply.github.com>
2024-08-29 16:55:09 -0300

    atualizazoes 2.0  referente ao navbar e home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,39 +1,41 @@
 // Importações necessárias para a página Home
 import "./Home.css";
 import React from 'react';
-import capa from '../img/capa.png';
+
+import post01 from '../img/post01-instagramFeed.jpg';
+import post02 from '../img/post02-instagramFeed.jpg';
+import post03 from '../img/post03-instagramFeed.jpg';
+import post04 from '../img/post04-instagramFeed.mp4';
 import { Link } from 'react-scroll';
 
 // Componente Home - seção inicial do site da Barbearia Diniz
 const Home = () => {
   return (
     // Seção principal da página inicial
-    <section className='container' id='home'>
-      <div className="container-capa d-flex justify-content-around align-items-center">
-        {/* Imagem de capa da barbearia */}
-        <img
-          src={capa}
-          alt="Capa da Barbearia Diniz"
-          width="45%"
-          height="45%"
-          className="d-inline-block capa bounceInLeft"
-        />
-        {/* Texto promocional e botão de agendamento */}
-        <div className='gatilho bounceInRight d-flex flex-column align-items-center'>
-          <h2>
-            Aqui é barba, cabelo e bigode! <br/>
-            Venha dar aquele tapa no visual
-          </h2>
-
-          {/* Link para a seção de agendamento */}
-          <a
-            className="btn btn-info"
-            id="botao"
-            href="https://www.barberbook.com.br/43"
-          >
-            Agende Online
-          </a>
+    <section id='home'>
+      <div id="carouselExample" className="carousel slide">
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <div className="aurelia"></div>
+            <img src={post01} className="img-card d-block w-100" alt="..."/>
+          </div>
+          <div className="carousel-item">
+            <div className="aurelia"></div>
+            <img src={post02} className="img-card d-block w-100" alt="..."/>
+          </div>
+          <div className="carousel-item">
+            <div className="aurelia"></div>
+            <img src={post03} className="img-card d-block w-100" alt="..."/>
+          </div>
         </div>
+        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button className="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Next</span>
+        </button>
       </div>
     </section>
   )
