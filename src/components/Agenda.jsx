c0308bfd0dd98c8136d9a18107865c6c6b1f12e9
José Luiz <82607322+JoseLuiz095@users.noreply.github.com>
2024-09-06 17:35:54 -0300

    Update Agenda.jsx

--- src/components/Agenda.jsx
+++ src/components/Agenda.jsx
@@ -17,17 +17,38 @@ const Agenda = () => {
       <h1>Agendamento</h1>
       <div className="container-agenda">
         <div className="map-container">
+        <article className="d-flex flex-column align-items-center">
+           <a href='#' className="contact-agendamento">
+           <i class="bi bi-whatsapp"></i>
+            <p>Whatsapp</p>
+           </a>
+
+           <a href='#' className="contact-agendamento">
+           <i class="bi bi-tools"></i>
+            <p>Trabalhe Conosco</p>
+           </a>
+
+           <a className="contact-agendamento">
+           <i class="bi bi-alarm"></i>
+            <p>Seg. à Sex. das 09h às 18h</p>
+           </a>
+
+           <a href='maito:dinizbarbershopp31@gmail.com' className="contact-agendamento">
+           <i class="bi bi-envelope"></i>
+            <p>dinizbarbershopp31@gmail.com</p>
+           </a>
+          </article>
+        </div>
+        <div className="d-flex flex-column justify-content-center align-items-center">
           {/* Iframe para exibição do Google Maps, mostrando a localização da barbearia */}
           <iframe 
             src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7481.622391024923!2d-43.671318!3d-20.349417000000003!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xa407bf5af350d3%3A0xfd1bd2073007f6fd!2sDiniz%20Barbershop!5e0!3m2!1spt-BR!2sbr!4v1724180864447!5m2!1spt-BR!2sbr"
             title="Localização da Barbearia Diniz" // Descrição do iframe para SEO e acessibilidade
             allowFullScreen="" // Permite que o mapa seja exibido em tela cheia
-            className='localização'
+            className='localizacao'
             loading="lazy" // Carrega o mapa de forma assíncrona, melhorando a performance do site
             referrerPolicy="no-referrer-when-downgrade" // Política de referência do iframe
           ></iframe>
-        </div>
-        <div className="btn-container">
           {/* Botão de agendamento que direciona para a página de agendamento */}
           <a href="https://www.barberbook.com.br/43" className="btn btn-info">Agendar Agora!</a>
         </div>
